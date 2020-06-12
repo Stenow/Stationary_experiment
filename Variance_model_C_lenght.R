33dca34647a17c590fda2b45be1215e4f7171e24
Rickard Stenow <ristenow@gmail.com>
2020-06-12 10:10:01 +0200

    Restructured the code to make it easier to read

--- Variance_model_C_lenght.R
+++ Variance_model_C_lenght.R
@@ -2,7 +2,9 @@ library(tidyverse) # run install.packages('tidyverse') in the console if you don
 library(TTR)  #run install.packages('TTR') in the console if you don't have it installed
 
 
-#Lugol_stats <- data.frame("Experiment"= c("EXP","STAT","LSTAT_NH4", "LSTAT_NO3"), "Mean_c_length" = c(5.3807, 3.8943, NaN, NaN), "Variance_c_length" = c(2.0677, 1.6253,NaN,NaN))
+
+# Imput settings ----------------------------------------------------------
+
 
 Mean_average  <-5.38     #The average used in the model
 Variance_sd <-2.07       #The sd used in the model
@@ -10,8 +12,19 @@ Number_of_samples <- 50  #The maximum number of samples
 Number_of_runs <- 100    #Number of times to run the model
 Number_of_samples_acctually_taken  <- NaN #The number of samples actually taken, adds a vertical line for them in the graph, remove it by setting it to NaN
 
-Raw_dataframe <- data.frame(matrix(0, ncol = Number_of_runs, nrow = Number_of_samples))  #Create a dataframe for the simulated data
 
+
+
+# Defining dataframes -----------------------------------------------------
+
+
+Raw_dataframe <- data.frame(matrix(0, ncol = Number_of_runs, nrow = Number_of_samples))  #Create a dataframe for the simulated data
+Sliding_mean <-data.frame(matrix(0, ncol = Number_of_runs, nrow = Number_of_samples)) #Create a dataframe for the sliding average
+Sliding_sd <-data.frame(matrix(0, ncol = Number_of_runs, nrow = Number_of_samples)) #Creating a dataframe for the slding sd, right now im not using the sd 
+
+
+
+# Create mock data --------------------------------------------------------
 
 
 for (i in 1:Number_of_samples) {
@@ -23,8 +36,8 @@ for (i in 1:Number_of_samples) {
 
 
 
-Sliding_mean <-data.frame(matrix(0, ncol = Number_of_runs, nrow = Number_of_samples)) #Create a dataframe for the sliding average
-Sliding_sd <-data.frame(matrix(0, ncol = Number_of_runs, nrow = Number_of_samples)) #Creating a dataframe for the slding sd, right now im not using the sd 
+
+# Calculated sliding average ----------------------------------------------
 
 
 
@@ -35,8 +48,20 @@ for (i in 1:Number_of_runs) {
   
 }
 
+
+
+
+# Reformat data for ggplot  -----------------------------------------------
+
+
 data_long <- gather(Sliding_mean)  #this rearange the data to a format that can acctually be plotted
 data_long$n_sample <- rep(1:Number_of_samples, times=Number_of_runs) # This adds the number of samples that is used for the average
+
+
+
+
+
+# Plotalot ----------------------------------------------------------------
 
 
 ggplot(data_long, aes(x=n_sample, y=value, color=key))+  #it is okay to get a error message that they removed rows equl to the number of runs (its becouse the first average is blank)
@@ -48,8 +73,12 @@ ggplot(data_long, aes(x=n_sample, y=value, color=key))+  #it is okay to get a er
   scale_y_continuous(limits = c(0,Mean_average+Variance_sd*2), n.breaks = 7)+
   xlab("Number of samples taken")+
   theme_classic()+
-  annotate("text", x = Number_of_samples/2, y = Mean_average+Variance_sd*1.5, label = paste("Mean value:",Mean_average,  #This adds the values assigned as the settings for the model, the pase comand ties it together
+  annotate("text", x = Number_of_samples/2, y = Mean_average+Variance_sd*1.5, label = paste("Mean value:",Mean_average,  #This adds the values assigned as the settings for the model, the paste comand ties it together
                                                                                          "\ \nVariance sd:", Variance_sd, #\ \n in a string makes a new row, note the spaces between \ \!
                                                                                          "\ \n Max number of samples:", Number_of_samples,
                                                                                          "\ \n Number of runs:", Number_of_runs))
   
+
+
+
+#Lugol_stats <- data.frame("Experiment"= c("EXP","STAT","LSTAT_NH4", "LSTAT_NO3"), "Mean_c_length" = c(5.3807, 3.8943, NaN, NaN), "Variance_c_length" = c(2.0677, 1.6253,NaN,NaN))
